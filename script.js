89cb38df1b77c312b9a12cc47e2157d069cef2ee
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-12-27 01:36:28 -0300

    Adicionar Botões de Navegação

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const sliderTabs = document.querySelectorAll(".slider-tab");
 const sliderIndicator = document.querySelector(".slider-indicator");
+const sliderControls = document.querySelector(".slider-controls");
 
 // Update the indicator height and width
 const updateIndicator = (tab, index) => {
     sliderIndicator.style.transform = `translateX(${tab.offsetLeft - 20}px)`;
     sliderIndicator.style.width = `${tab.getBoundingClientRect().width}px`;
+
+    // Calculate the scroll position and scroll smoothly
+    const scrollLeft = sliderTabs[index].offsetLeft - sliderControls.offsetWidth / 2 + sliderTabs[index].offsetWidth / 2;
+    sliderControls.scrollTo({ left: scrollLeft, behavior: "smooth" });
 }
 
 // Initialize swiper instance
@@ -12,6 +17,10 @@ const swiper = new Swiper('.slider-container', {
   effect: "fade", // slide
   speed: 1300,
   // autoplay: { delay: 4000 }
+  navigation: {
+    prevEl: "#slide-prev",
+    nextEl: "#slide-next"
+  },
   on: {
     // Update the indicator on slide change
     slideChange: () => {
