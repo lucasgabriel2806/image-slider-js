1f658257c82e74bd195c4577d5007886aef12a7d
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-12-27 01:09:01 -0300

    Implementar paginação

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
+const sliderTabs = document.querySelectorAll(".slider-tab");
+const sliderIndicator = document.querySelector(".slider-indicator");
+
+// Update the indicator height and width
+const updateIndicator = (tab, index) => {
+    sliderIndicator.style.transform = `translateX(${tab.offsetLeft - 20}px)`;
+    sliderIndicator.style.width = `${tab.getBoundingClientRect().width}px`;
+}
+
 // Initialize swiper instance
 const swiper = new Swiper('.slider-container', {
   effect: "fade", // slide
   speed: 1300,
-  autoplay: { delay: 4000 }
+  // autoplay: { delay: 4000 }
+  on: {
+    // Update the indicator on slide change
+    slideChange: () => {
+        const currentTabIndex = [...sliderTabs].indexOf(sliderTabs[swiper.activeIndex]);
+        updateIndicator(sliderTabs[swiper.activeIndex], currentTabIndex);
+    }
+  }
 });
+
+// Update the slide and indicator on tab click
+sliderTabs.forEach((tab, index) => {
+    tab.addEventListener("click", () => {
+        swiper.slideTo(index);
+        updateIndicator(tab, index);
+    });
+});
+
+updateIndicator(sliderTabs[0], 0);
+window.addEventListener("rezise", () => updateIndicator(sliderTabs[swiper.activeIndex], 0));
